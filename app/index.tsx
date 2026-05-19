ff854de9301148e3e01c9d55c51df85bc56340bd
Hamza11067 <hamzakhalid1067@gmail.com>
2026-05-19 10:50:19 +0500

    Installing Native wind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function Index() {
+const index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <SafeAreaView>
+      <Text className='text-4xl text-center'>index</Text>
+    </SafeAreaView>
+  )
 }
+
+export default index
